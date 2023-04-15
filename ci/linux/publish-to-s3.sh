6707b452d86fd778ff6a8e028b43114c2e1987e0
John Haynes <tundranerd@gmail.com>
2023-04-15 14:42:17 -0600

    Shared ci resources

---
+++ ci/linux/publish-to-s3.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -x
+SOURCE=$1
+DIR=$(dirname "$SOURCE")
+SOURCE=$(basename "$SOURCE")
+TARGET_NAME=$SOURCE
+pushd "$DIR" || exit 1
+PACKAGE="${TARGET_NAME}-${GIT_TAG}.tar.gz"
+BUCKET_NAME=${BUCKET_NAME:-febio-packages}
+REF_NAME=${REF_NAME:-$(git rev-parse --abbrev-ref HEAD)}
+PACKAGE_NAME=${PACKAGE_NAME:-febio}
+OS=${OS:-Linux}
+tar -czf "$PACKAGE" "$SOURCE"
+TARGET="s3://${BUCKET_NAME}/${PACKAGE_NAME}/${REF_NAME}/${OS}/${PACKAGE}"
+aws s3 cp "$PACKAGE" "$TARGET"
+popd || exit 1
