4de9afaba0a439f9103d37b8493817d446ef1e0d
John Haynes <tundranerd@gmail.com>
2023-04-15 18:30:32 -0600

    Add OS to package name

--- linux/publish-to-s3.sh
+++ linux/publish-to-s3.sh
@@ -5,11 +5,11 @@ DIR=$(dirname "$SOURCE")
 SOURCE=$(basename "$SOURCE")
 TARGET_NAME=$SOURCE
 pushd "$DIR" || exit 1
-PACKAGE="${TARGET_NAME}-${GIT_TAG}.tar.gz"
+OS=${OS:-Linux}
+PACKAGE="${TARGET_NAME}-${GIT_TAG}-${OS}.tar.gz"
 BUCKET_NAME=${BUCKET_NAME:-febio-packages}
 REF_NAME=${REF_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 PACKAGE_NAME=${PACKAGE_NAME:-febio}
-OS=${OS:-Linux}
 tar -czf "$PACKAGE" "$SOURCE"
 TARGET="s3://${BUCKET_NAME}/${PACKAGE_NAME}/${REF_NAME}/${OS}/${PACKAGE}"
 aws s3 cp "$PACKAGE" "$TARGET"
