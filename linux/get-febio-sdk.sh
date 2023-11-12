df62b78482064a1fc0b0eafb0cb0d430e1e28c6e
John Haynes <tundranerd@gmail.com>
2023-11-11 20:50:20 -0700

    Lookup sdk by version

--- linux/get-febio-sdk.sh
+++ linux/get-febio-sdk.sh
@@ -2,8 +2,25 @@
 set -o errexit
 set -o verbose
 
-BUCKET=${BUCKET:-febio-packages}
-PACKAGE_NAME=${PACKAGE_NAME-febio}
-SDK_S3_URI="s3://$BUCKET/$PACKAGE_NAME/$FEBIO_SDK_BRANCH/$OS/$PACKAGE"
-aws s3 cp "$SDK_S3_URI" .
-tar xzf "$PACKAGE"
+set -x
+
+OS="${OS}"
+BUCKET="${BUCKET:-febio-packages}"
+PACKAGE="${PACKAGE:-febio4-sdk}"
+PACKAGE_NAME="${PACKAGE_NAME:-febio4}"
+FEBIO_SDK_BRANCH="${FEBIO_SDK_BRANCH}"
+FEBIO_VERSION="${FEBIO_VERSION:-v4}"
+
+PACKAGE_PREFIX="${PACKAGE_NAME}/${FEBIO_SDK_BRANCH}/${OS}/"
+PACKAGE_SEARCH="${PACKAGE}-${FEBIO_VERSION}"
+
+PACKAGE_URI=$(aws --output json s3api list-objects \
+	--bucket "$BUCKET" \
+	--prefix "$PACKAGE_PREFIX" \
+	--query "Contents[?contains(Key, '$PACKAGE_SEARCH')]" \
+	| jq -r -e '. | max_by(.LastModified) | .Key')
+
+
+ARCHIVE="${PACKAGE_URI##*/}"
+aws s3 cp "s3://$BUCKET/$PACKAGE_URI" .
+tar xzf "$ARCHIVE"
