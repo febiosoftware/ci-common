745cc9c219a89785592fd1163b950ff344da5cad
John Haynes <tundranerd@gmail.com>
2024-04-20 13:32:00 -0600

    Tag previous upload for expiration

--- linux/get-febio-sdk.sh
+++ linux/get-febio-sdk.sh
@@ -8,8 +8,8 @@ OS="${OS}"
 BUCKET="${BUCKET:-febio-packages}"
 PACKAGE="${PACKAGE:-febio4-sdk}"
 PACKAGE_NAME="${PACKAGE_NAME:-febio4}"
-FEBIO_SDK_BRANCH="${FEBIO_SDK_BRANCH}"
-FEBIO_VERSION="${FEBIO_VERSION:-v4}"
+FEBIO_SDK_BRANCH="${FEBIO_SDK_BRANCH:-develop}"
+FEBIO_VERSION="${FEBIO_VERSION:-v}"
 
 PACKAGE_PREFIX="${PACKAGE_NAME}/${FEBIO_SDK_BRANCH}/${OS}/"
 PACKAGE_SEARCH="${PACKAGE}-${FEBIO_VERSION}"
@@ -17,10 +17,10 @@ PACKAGE_SEARCH="${PACKAGE}-${FEBIO_VERSION}"
 PACKAGE_URI=$(aws --output json s3api list-objects \
 	--bucket "$BUCKET" \
 	--prefix "$PACKAGE_PREFIX" \
-	--query "Contents[?contains(Key, '$PACKAGE_SEARCH')]" \
+	--query "reverse(sort_by(Contents,&LastModified)) && Contents[?contains(Key, '$PACKAGE')]" \
 	| jq -r -e '. | max_by(.LastModified) | .Key')
 
 
 ARCHIVE="${PACKAGE_URI##*/}"
-aws s3 cp "s3://$BUCKET/$PACKAGE_URI" .
-tar xzf "$ARCHIVE"
+#aws s3 cp "s3://$BUCKET/$PACKAGE_URI" .
+#tar xzf "$ARCHIVE"
