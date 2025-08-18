cb4f864e1f42802beb58ba4d213ca6e6abe8d6ea
michaelrossherron <michaelrossherron@gmail.com>
2025-08-18 14:01:48 -0600

    Updated gather plugins script to write out plugin versions to file

--- gather-plugins.py
+++ gather-plugins.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, json
 
 os.mkdir("plugins")
 
@@ -8,3 +8,49 @@ for root, dirs, files in os.walk("pluginRepos", followlinks=True):
             filename = name.split("/")[-1].split("\\")[-1]
 
             shutil.copy2(os.path.join(root,name), os.path.join("plugins", filename))
+
+def getVersion(path):
+    if not path:
+        return None
+
+    major = minor = patch = None
+    with open(path, "r") as f:
+        for line in f:
+            if "#define VERSION" in line:
+                major = line.split()[-1].strip()
+            elif "#define SUBVERSION" in line:
+                minor = line.split()[-1].strip()
+            elif "#define SUBSUBVERSION" in line:
+                patch = line.split()[-1].strip()
+
+    if major and minor and patch:
+        return f"{major}.{minor}.{patch}"
+    
+    return None
+
+versionInfo = {}
+
+# FEBio version
+versionInfo["febio"] = getVersion("febio4-sdk/include/FEBioLib/version.h")
+
+# Find version for each plugin
+for dirName in os.listdir("pluginRepos"):
+    dirPath = os.path.join("pluginRepos", dirName)
+    
+    if os.path.isdir(dirPath):
+        versionHeader = None
+        for root, dirs, files in os.walk(".", followlinks=True):
+            if name.endswith("version.h"):
+                versionHeader = os.path.join(root, name)
+                break
+            
+            if versionHeader:
+                break
+        
+        if versionHeader:
+            versionInfo[dirName] = getVersion(os.path.join(dirPath, versionHeader))
+        else:
+            print(f"Unable to find version header for {dirPath}")
+
+with open("plugins/versions.json", "w") as f:
+    json.dump(versionInfo, f)
