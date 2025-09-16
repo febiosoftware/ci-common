9962eab32d57299da1addbd7df08dbb1640f8519
michaelrossherron <michaelrossherron@gmail.com>
2025-09-16 11:57:13 -0600

    Updated sdk package name

--- gather-plugins.py
+++ gather-plugins.py
@@ -39,7 +39,7 @@ versionInfo = {}
 
 FEBIO_SDK = os.getenv("FEBIO_SDK")
 if FEBIO_SDK is None:
-    FEBIO_SDK = "febio4-sdk"
+    FEBIO_SDK = "sdk"
 
 # FEBio version
 versionInfo["febio"] = getVersion(FEBIO_SDK + "/include/FEBioLib/version.h")
